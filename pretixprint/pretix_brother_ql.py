c3b942c052026e1c5ea11d19ca5cdc4c8a25b3a9
Hauke Webermann <hauke.webermann@esd.eu>
2018-09-12 10:56:45 +0200

    Changed with some PIP8 improvements

--- pretixprint/pretix_brother_ql.py
+++ pretixprint/pretix_brother_ql.py
@@ -3,8 +3,19 @@
 __author__ = "Hauke Webermann"
 __copyright__ = "Copyright 2018, webermann.net"
 __license__ = "MIT"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __email__ = "hauke@webermann.net"
+
+from brother_ql import BrotherQLRaster
+from brother_ql.conversion import convert
+from brother_ql.backends.helpers import send
+
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+from nameparser import HumanName
+
 
 CONFIG = {
     'printer': 'QL-810W',
@@ -12,43 +23,25 @@ CONFIG = {
     'backend': 'pyusb',
 }
 
-import sys
-#reload(sys)
-#sys.setdefaultencoding('utf-8')
+imgSize = (900, 540)
+fillBlack = (0, 0, 0)
+fillWhite = (255, 255, 255)
 
-from pprint import pprint
-
-from brother_ql import BrotherQLRaster, create_label
-from brother_ql.conversion import convert
-from brother_ql.backends.helpers import send
-
-import PIL
-from PIL import ImageFont
-from PIL import Image
-from PIL import ImageDraw
-
-from nameparser import HumanName
-
-imgSize = (900,540)
-fillBlack = (0,0,0)
-fillWhite = (255,255,255)
 
 def printCenter(d, y, text, font):
     textSize = d.textsize(text, font)
     x = (imgSize[0] - textSize[0]) / 2
     d.multiline_text((x, y), text, fillBlack, font=font)
 
+
 def printLabel(user, eventName, count=1):
     FONT_NAME = "/usr/share/fonts/truetype/DejaVuSans.ttf"
-    f20 = ImageFont.truetype(FONT_NAME, 20)
     f25 = ImageFont.truetype(FONT_NAME, 25)
-    f30 = ImageFont.truetype(FONT_NAME, 30)
     f40 = ImageFont.truetype(FONT_NAME, 40)
-    f50 = ImageFont.truetype(FONT_NAME, 50)
     f80 = ImageFont.truetype(FONT_NAME, 80)
     f100 = ImageFont.truetype(FONT_NAME, 100)
 
-    img=Image.new("RGB", imgSize, fillWhite)
+    img = Image.new("RGB", imgSize, fillWhite)
     draw = ImageDraw.Draw(img)
 
     name = HumanName(user['Name'])
@@ -58,9 +51,9 @@ def printLabel(user, eventName, count=1):
         ticket[0] += ' ' + ticket[1]
     
     mitarbeiter = False
-    if ('Mitarbeiter' in user['Tickets']):
+    if 'Mitarbeiter' in user['Tickets']:
         mitarbeiter = True
-    if ('Free' in user['Tickets']):
+    if 'Free' in user['Tickets']:
         mitarbeiter = True
     
     printCenter(draw, 50, name.first, f100)
@@ -78,8 +71,7 @@ def printLabel(user, eventName, count=1):
     if 'Seminare und Workshops' in user:
         seminar = user['Seminare und Workshops'].split('(')
         printCenter(draw, 485, seminar[0], f25)
-    
-    draw = ImageDraw.Draw(img)
+
     img.save('tmp.png')
 
     qlr = BrotherQLRaster(CONFIG['printer'])
@@ -87,6 +79,7 @@ def printLabel(user, eventName, count=1):
     convert(qlr, ['tmp.png'], '54', cut=True, dither=False, compress=True, red=False, dpi_600=False, hq=True, rotate=90)
     for i in range(count):
         send(instructions=qlr.data, printer_identifier=CONFIG['identifier'], backend_identifier=CONFIG['backend'], blocking=True)   
+
 
 if __name__ == "__main__": 
     user = {
--- pretixprint/pretix_manual_print.py
+++ pretixprint/pretix_manual_print.py
@@ -3,19 +3,20 @@
 __author__ = "Hauke Webermann"
 __copyright__ = "Copyright 2018, webermann.net"
 __license__ = "MIT"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __email__ = "hauke@webermann.net"
 
 import logging
+import signal
+from pretix_brother_ql import printLabel
+
 logging.basicConfig(level=logging.ERROR)
 
-import signal
-from pprint import pprint
-from pretix_brother_ql import printLabel
 
 def sigint_handler(signum, frame):
     exit()
- 
+
+
 def main():
     signal.signal(signal.SIGINT, sigint_handler)    
     
@@ -36,6 +37,7 @@ def main():
         printLabel(user, 'Connect 2018', 2)
 
     exit()
+
 
 if __name__ == "__main__": 
     main()
